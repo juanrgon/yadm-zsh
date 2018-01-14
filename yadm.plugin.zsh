166c113fd4084f8878bd1063257fcc2cc0ee5c34
Juan Gonzalez <jrg2156@gmail.com>
2018-01-14 00:23:40 -0500

    Check yadm status before each command execution

--- yadm.plugin.zsh
+++ yadm.plugin.zsh
@@ -1,3 +1,8 @@
-if [[ $(yadm status -s) ]]; then
-    echo "There are local configuration changes. Yadm sync required."
-fi
+_yadm_status () {
+    if [[ $(yadm status -s) ]]; then
+        echo "There are local configuration changes. Yadm sync required."
+    fi
+}
+
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec _yadm_status
