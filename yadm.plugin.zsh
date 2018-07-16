01e2dbfbafe5b344f5f523f535dde9a493e4c80c
Juan Gonzalez <jrg2156@gmail.com>
2018-07-16 17:48:41 -0400

    Rely on ~/.yadm/.status for yadm repo status

--- yadm.plugin.zsh
+++ yadm.plugin.zsh
@@ -1,35 +1,15 @@
-# YADM_STATUS values
-# 0 - nothing to do
-# 1 - yadm has unsaved changes
-# 2 - yadm changes need to be pushed to the remote
-export YADM_STATUS=0
-
-_update_yadm_status () {
-    local branch_name ahead
-    if [[ $(yadm status -s) ]]; then
-        message='%B%F{magenta}There are local configuration changes. Yadm sync required.%f%b'
-        YADM_STATUS=1
-    else
-        branch_name=$(yadm symbolic-ref --short HEAD 2>/dev/null)
-        ahead=$(yadm rev-list "${branch_name}"@{upstream}..HEAD 2>/dev/null | wc -l)
-        if (( ahead )); then
-            YADM_STATUS=2
-        else
-            YADM_STATUS=0
-        fi
-    fi
-}
-
 _prompt_yadm_status () {
-    if [[ $YADM_STATUS -eq 1 ]]; then
-        print -P '%B%F{magenta}There are local configuration changes. Yadm sync required.%f%b'
-    elif [[ $YADM_STATUS -eq 2 ]]; then
-        print -P '%B%F{magenta}Run yadm push.%f%b'
-    fi
+    case $(cat ~/.yadm/.status) in
+        (1)
+            print -P '%B%F{magenta}There are local configuration changes. Yadm sync required.%f%b'
+            ;;
+        (2)
+            print -P '%B%F{magenta}Run yadm push.%f%b'
+            ;;
+    esac
 }
 
 autoload -Uz add-zsh-hook
-add-zsh-hook periodic _update_yadm_status
 add-zsh-hook precmd _prompt_yadm_status
 
 # Aliases
